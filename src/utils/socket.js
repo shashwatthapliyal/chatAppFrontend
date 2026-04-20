c1d4a0dba3e4bca7d2fa3109e627af0a30a32290
ShashwatTH <shashwatthapliyal021@gmail.com>
2026-04-20 10:27:46 +0530

    updated socket.io config

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,24 +2,8 @@ import { BASE_URL } from "./constants";
 import { io } from "socket.io-client";
 
 
-// Add auth in the message
+const socket = location.hostname === "localhost"
+    ? io(BASE_URL)
+    : io("/", { path: "/api/socket.io" });
 
-// function getCookie(name) {
-//     return document.cookie
-//         .split("; ")
-//         .find((row) => row.startsWith(name + "="))
-//         ?.split("=")[1];
-
-// }
-
-// const token = getCookie("token");
-
-// console.log(token)
-
-const socket = io(BASE_URL//, {
-//     token,
-//  }
-
-);
 export default socket;
-
